29833251f2fdb5cea79e73b7cf20b328789c1da9
Sam Jackson <sjacks26@gmail.com>
2017-12-06 11:28:17 -0500

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,4 @@
+save_dir = '' # Directory path to save html files from website
+target_url = '' # URL for the first page to start the recursive downloading
+headless = True
+error_file = save_dir + 'errors.txt'
